5c847830179aba46f70dd75c20fa04269a775cbb
tuntikorn <tuntikornwisuttikul@gmail.com>
2020-12-08 17:43:35 +0700

    [Update] icon text style

--- app/components/header/header.props.ts
+++ app/components/header/header.props.ts
@@ -42,6 +42,11 @@ export interface HeaderProps {
    */
   titleStyle?: TextStyle
 
+  leftIconText?: string
+  rightIconText?: string
+  leftIconTextStyle?: TextStyle
+  rightIconTextStyle?: TextStyle
+
   rightIconReal?: boolean
   rightIconName?: string
   rightIconSize?: number
