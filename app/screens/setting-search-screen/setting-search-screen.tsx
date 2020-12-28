ffc16192cea05a1e52e65325f8131aa5f3026521
Kittichai <king_atillart_sword3@hotmail.com>
2020-12-28 11:04:39 +0700

    [Update] setting search screen

--- app/screens/setting-search-screen/setting-search-screen.tsx
+++ app/screens/setting-search-screen/setting-search-screen.tsx
@@ -4,11 +4,19 @@ import { Dimensions, TextStyle, View, ViewStyle } from 'react-native'
 import { Button, Checkbox, Text } from '../../components'
 import { color, spacing } from '../../theme'
 import { useNavigation } from '@react-navigation/native'
+import { ScrollView } from 'react-native-gesture-handler'
 
+const deviceWidht = Dimensions.get('window').width / 2
+const marginPercent = 1
 interface MENU {
+  id?: number
   topic?: string
+  showSubColumn?: number
+  isChecked?: boolean
   subMenu?: {
+    id?: number
     label?: string
+    isChecked?: boolean
   }[]
 }
 
@@ -21,10 +29,24 @@ const SEARCH_ITEM_ROOT: ViewStyle = {
   // backgroundColor: "lightblue",
   borderWidth: 1,
   borderColor: color.disable,
-  padding: spacing[3]
 }
 const ITEM: ViewStyle = {
-
+  borderBottomColor: color.line,
+  borderBottomWidth: 1,
+  paddingTop: spacing[2],
+}
+const SUB_MENU: ViewStyle = {
+  flexDirection: 'row',
+  // justifyContent: 'center',
+  flexWrap: 'wrap',
+  paddingVertical: spacing[3]
+}
+const ITEM_SUB_MENU: ViewStyle = {
+  backgroundColor: color.transparent,
+  borderWidth: 1,
+  borderRadius: deviceWidht,
+  marginVertical: spacing[1],
+  marginHorizontal: `${marginPercent}%`,
 }
 const TOPIC: TextStyle = {
   fontSize: 12,
@@ -35,8 +57,6 @@ const CONTENT: TextStyle = {
 const ROW: ViewStyle = {
   alignItems: "center",
   flexDirection: 'row',
-  borderBottomColor: color.line,
-  borderBottomWidth: 1,
 }
 const BUTTON_ROOT: ViewStyle = {
   flex: 1,
@@ -47,7 +67,7 @@ const BUTTON_ROOT: ViewStyle = {
 const BUTTON_CONFIRM: ViewStyle = {
   backgroundColor: color.primary,
   width: '90%',
-  borderRadius: Dimensions.get('screen').width / 2,
+  borderRadius: deviceWidht,
 }
 const BUTTON_CONFIRM_TEXT: TextStyle = {
   color: color.textWhite,
@@ -58,130 +78,198 @@ const BUTTON_CONFIRM_TEXT: TextStyle = {
 
 const MENUS: Array<MENU> = [
   {
+    id: 1,
     topic: 'ประเภทรถ',
+    showSubColumn: 3,
+    isChecked: false,
     subMenu: [
       {
-        label: 'รถ 4 ล้อ'
+        id: 11,
+        label: 'รถ 4 ล้อ',
+        isChecked: false,
       },
       {
-        label: 'รถ 6 ล้อ'
+        id: 12,
+        label: 'รถ 6 ล้อ',
+        isChecked: false,
       },
       {
-        label: 'รถ 10 ล้อ'
+        id: 13,
+        label: 'รถ 10 ล้อ',
+        isChecked: false,
       },
     ]
   },
   {
+    id: 2,
     topic: 'จำนวนรถ',
+    showSubColumn: 3,
+    isChecked: false,
     subMenu: [
       {
-        label: '1-2 คัน'
+        id: 21,
+        label: '1-2 คัน',
+        isChecked: false,
       },
       {
-        label: '3-4 คัน'
+        id: 22,
+        label: '3-4 คัน',
+        isChecked: false,
       },
       {
-        label: 'มากกว่า 4 คัน'
+        id: 23,
+        label: 'มากกว่า 4 คัน',
+        isChecked: false,
       },
     ]
   },
   {
+    id: 3,
     topic: 'ประเภทสินค้า',
+    showSubColumn: 2,
+    isChecked: false,
     subMenu: [
       {
-        label: 'สินค้าเกษตร'
+        id: 31,
+        label: 'สินค้าเกษตร',
+        isChecked: false,
       },
       {
-        label: 'สินค้าเกษตร'
+        id: 32,
+        label: 'สินค้าเกษตร',
+        isChecked: false,
       },
       {
-        label: 'สินค้าเกษตร'
+        id: 33,
+        label: 'สินค้าเกษตร',
+        isChecked: false,
       },
       {
-        label: 'สินค้าเกษตร'
+        id: 34,
+        label: 'สินค้าเกษตร',
+        isChecked: false,
       },
       {
-        label: 'สินค้าเกษตร'
+        id: 35,
+        label: 'สินค้าเกษตร',
+        isChecked: false,
       },
       {
-        label: 'สินค้าเกษตร'
+        id: 36,
+        label: 'สินค้าเกษตร',
+        isChecked: false,
       },
     ]
   },
   {
+    id: 4,
     topic: 'น้ำหนัก',
+    showSubColumn: 2,
+    isChecked: false,
     subMenu: [
       {
-        label: '1-5 ตัน'
+        id: 41,
+        label: '1-5 ตัน',
+        isChecked: false,
       },
       {
-        label: '5-10 ตัน'
+        id: 42,
+        label: '5-10 ตัน',
+        isChecked: false,
       },
     ]
   },
 ]
 
+const initialState = {
+  settings: MENUS
+}
+
 export const SettingSearchScreen = observer(function SettingSearchScreen() {
   const navigation = useNavigation()
-  const goBack = () => navigation.goBack()
 
-  const [searchByCategoryCar, setSearchByCategoryCar] = useState(false)
-  const [searchByPrice, setSearchByPrice] = useState(false)
+  const [{ settings }, setState] = useState(initialState)
 
-  const SubMenu = ({ label, mainIndex, subIndex }): ReactElement => (
-    <Button
-      key={`${mainIndex}${subIndex}`}
+  const onClick = (id: number, isChecked: boolean) => {
+    const newMenu = settings.map(menu => {
+      if (menu.id !== id) return menu
+      const subMenu = menu.subMenu && menu.subMenu.length ? menu.subMenu.map(item => {
+        return { ...item, isChecked: !isChecked }
+      }) : []
+      return { ...menu, isChecked: !isChecked, subMenu }
+    })
+
+    setState(prevState => ({
+      ...prevState,
+      settings: newMenu
+    }))
+  }
+
+  const onSelect = (subId: number, mainId: number, isChecked: boolean) => {
+    const newMenu = settings.map(menu => {
+      if (menu.id !== mainId) return menu
+      const subMenu = menu.subMenu && menu.subMenu.length ? menu.subMenu.map(item => {
+        if (item.id !== subId) return item
+        return { ...item, isChecked: !isChecked }
+      }) : []
+      const unCheckedAll = subMenu.filter(menu => menu.isChecked === true)
+      return { ...menu, isChecked: !!unCheckedAll.length, subMenu }
+    })
+
+    setState(prevState => ({
+      ...prevState,
+      settings: newMenu
+    }))
+  }
+
+  const SubMenu = ({ id, label, isChecked, mainIndex, percentWidth }): ReactElement => {
+
+    return (<Button
+      key={id}
       text={label}
       style={{
-        backgroundColor: color.transparent,
-        borderWidth: 1,
-        borderColor: color.primary,
-        borderRadius: Dimensions.get('screen').width / 2,
+        ...ITEM_SUB_MENU,
+        borderColor: isChecked === true ? color.primary : color.line,
+        flexBasis: `${percentWidth}%`,
       }}
       textStyle={{
         color: color.textBlack,
+        fontSize: 12
       }}
-    />
-  )
+      onPress={() => onSelect(id, mainIndex, isChecked)}
+    />)
+  }
 
   return (
     <View style={CONTAINER}>
       <View style={SEARCH_ITEM_ROOT}>
+        <ScrollView style={{ padding: spacing[3] }}>
 
-        {
-          MENUS.map((menu, index) => {
-            return (
-              <View style={ITEM} key={index} >
-                <View style={ROW}>
-                  <Checkbox value={searchByCategoryCar} onToggle={() => setSearchByCategoryCar(!searchByCategoryCar)} />
-                  <Text text={menu.topic} style={CONTENT} />
+          {
+            settings.length && settings.map((menu, index) => {
+              return (
+                <View style={ITEM} key={index} >
+                  <View style={ROW}>
+                    <Checkbox value={menu.isChecked} onToggle={() => onClick(menu.id, menu.isChecked)} />
+                    <Text text={menu.topic} style={CONTENT} onPress={() => onClick(menu.id, menu.isChecked)} />
+                  </View>
+                  <View style={SUB_MENU}>
+                    {
+                      menu.subMenu && menu.subMenu.map((subMenu, i) => {
+                        const percentWidth = (100 - (menu.showSubColumn * (marginPercent * 2))) / menu.showSubColumn
+                        const { id, label, isChecked } = subMenu
+                        return (<SubMenu key={id} id={id} label={label} isChecked={isChecked} mainIndex={menu.id} percentWidth={percentWidth} />)
+                      })
+                    }
+                  </View>
                 </View>
-                <View style={{ flexDirection: 'row' }}>
-                  {
-                    menu.subMenu && menu.subMenu.map((subMenu, i) => {
-                      return (<SubMenu key={`${index}${i}`} label={subMenu.label} mainIndex={index} subIndex={i} />)
-                    })
-                  }
-                </View>
-              </View>
-            )
-          })
-        }
+              )
+            })
+          }
 
-        {/* <View style={ITEM}>
-          <View style={ROW}>
-            <Checkbox value={searchByCategoryCar} onToggle={() => setSearchByCategoryCar(!searchByCategoryCar)} />
-            <Text text={'กรุณาเลือกประเภทรถขนส่ง'} style={CONTENT} />
-          </View>
-        </View>
-        <View style={ITEM}>
-          <View style={ROW}>
-            <Checkbox value={searchByPrice} onToggle={() => setSearchByPrice(!searchByPrice)} />
-            <Text text={'ช่วงราคา'} style={CONTENT} />
-            <Text text={'ช่วงราคา'} style={CONTENT} />
-          </View>
-        </View> */}
+        </ScrollView>
       </View>
+
       <View style={BUTTON_ROOT}>
         <Button
           testID="setting-search-confirm"
