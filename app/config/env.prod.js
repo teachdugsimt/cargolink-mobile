dedc3a7d0e41f2419e3378c140a44a89b6f6c1b4
Katanyoo Ubalee <ubalee.k@gmail.com>
2021-02-03 08:57:03 +0700

    [Update] myjob screen - add new tab

--- app/config/env.prod.js
+++ app/config/env.prod.js
@@ -1,4 +1,4 @@
-const API_URL = "https://trucking.cargolink.co.th/"
+const API_URL = "https://staging.cargolink.co.th/"
 const GOOGLE_API_KEY = "AIzaSyD_xZbQQVruH1NWLqCE2kgSWBPoWH7l3Sw"
 module.exports = {
   API_URL,
