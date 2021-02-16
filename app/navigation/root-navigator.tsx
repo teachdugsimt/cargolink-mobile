5333df489233682d68cdf9a9c9384e8995126043
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-02-16 22:08:36 +0700

    [Merge] android back 2

--- app/navigation/root-navigator.tsx
+++ app/navigation/root-navigator.tsx
@@ -37,7 +37,7 @@ const RootStack = () => {
           gestureEnabled: true,
           // stackPresentation: "modal",
         }}
-        initialRouteName={"primaryStack"}
+        // initialRouteName={"primaryStack"}
       >
         <Stack.Screen
           name="primaryStack"
