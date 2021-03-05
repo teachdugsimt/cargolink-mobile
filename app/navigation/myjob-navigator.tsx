49531bf902578844ceea2aca253805f6b13c94e2
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-03-05 15:00:51 +0700

    [Clean] old top tab

--- app/navigation/myjob-navigator.tsx
+++ app/navigation/myjob-navigator.tsx
@@ -7,7 +7,6 @@
 import React, { } from "react"
 import { observer } from 'mobx-react-lite'
 import { createNativeStackNavigator } from "react-native-screens/native-stack"
-// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { HeaderCenter, HeaderLeft, TabBar } from "../components"
 import {
   FeedbackScreen, JobDetailScreen, MyJobScreen, ReceivePointScreen, PostJobScreen,
@@ -17,30 +16,6 @@ import {
   CarrierProfileScreen, TruckDetailOnlyScreen
 } from "../screens"
 import { color } from "../theme"
-// import { translate } from "../i18n"
-// import { useStores } from '../models'
-
-// const Tab = createMaterialTopTabNavigator();
-
-// const MyjobTab = observer(function MyjobTab() {
-//   const { versatileStore } = useStores()
-//   return (
-//     <Tab.Navigator
-//       initialRouteName={'new'}
-//       tabBarOptions={{
-//         style: { backgroundColor: color.primary, borderColor: color.line, borderBottomColor: color.line },
-//         activeTintColor: color.textWhite,
-//         inactiveTintColor: color.textBlack,
-//         labelStyle: { fontFamily: "Kanit-Medium", fontSize: 16 },
-//         indicatorStyle: { backgroundColor: color.textBlack }
-//       }}
-//     >
-//       <Tab.Screen name="new" options={{ tabBarLabel: translate('myJobScreen.workOpen', { locale: versatileStore.language }) }} component={MyJobScreen} initialParams={{ status: 0 }} />
-//       <Tab.Screen name="inprogress" options={{ tabBarLabel: translate('myJobScreen.workInProgress', { locale: versatileStore.language }) }} component={MyJobScreen} initialParams={{ status: 1 }} />
-//       <Tab.Screen name="done" options={{ tabBarLabel: translate('myJobScreen.workDone', { locale: versatileStore.language }) }} component={MyJobScreen} initialParams={{ status: 2 }} />
-//     </Tab.Navigator>
-//   );
-// })
 /**
  * This type allows TypeScript to know what routes are defined in this navigator
  * as well as what properties (if any) they might take when navigating to them.
