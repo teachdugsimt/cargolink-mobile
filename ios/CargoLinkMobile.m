590a1fe49671b575c1d542ed25bb85332b3c9c86
Katanyoo Ubalee <ubalee.k@gmail.com>
2021-05-10 15:43:16 +0700

    chore(xcode): upgrade script for xcode new version

---
+++ ios/CargoLinkMobile.m
@@ -0,0 +1,10 @@
+//
+//  CargoLinkMobile.m
+//  CargoLinkMobile
+//
+//  Created by Katanyoo Ubalee on 10/5/2564 BE.
+//
+
+#import "React/RCTBridgeModule.h"
+@interface RCT_EXTERN_MODULE(CargoLinkMobile, NSObject)
+@end
